19d2cb1ddc99fdf286481865a750b0eb2d24947f
Dxlgnds <110533724+Dxlgnds@users.noreply.github.com>
2023-07-09 20:30:26 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
           </nav>
           <div className="text-center p-10">
 
-          <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">Dinan Legends</h2>
+          <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">Muhammad Dinan</h2>
           <h3 className="text-2xl py-2 dark:text-white md:text-3xl">Students and developer</h3>
           <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl max-w-xl mx-auto">Lazy students try to understanding Programming Language.</p>
 
