a50e2d78010ca6ea80d6da5812256d31df5cdf33
Dxlgnds <110533724+Dxlgnds@users.noreply.github.com>
2023-08-20 17:03:09 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,9 +27,9 @@ export default function Home() {
       <main className="bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="text-gray-200 text-2xl font-burtons">dinannn</h1>
+            <h1 className="text-black text-2xl font-burtons">dinannn</h1>
             <ul className="flex items-center">
-              <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl" /></li>
+              <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="fill-black cursor-pointer text-2xl" /></li>
               <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 px-2 rounded-md ml-8 font-medium" href="#">Resume</a></li>
             </ul>
           </nav>
